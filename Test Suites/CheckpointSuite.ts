e3305067eae4e7efe7b40c99bd71686543777a07
EY-Ashish <ashish.desai1@in.ey.com>
2024-05-26 10:54:03 +0530

    Commit

---
+++ Test Suites/CheckpointSuite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>CheckpointSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>15ff1502-a0a5-4805-b837-94399f2207dc</testSuiteGuid>
+</TestSuiteEntity>
